666cf49d3f69a727382e85816c9b7d36d4062571
Andressa Silva <silva.13579.2468@gmail.com>
2026-04-20 17:56:05 -0300

    FIX: Add init.py

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="react-scripts" />
+/// <reference types="jest" />
 
+/** Side-effect and CSS module imports (VS Code/TS when react-scripts types are not picked up). */
+declare module '*.css';
 
